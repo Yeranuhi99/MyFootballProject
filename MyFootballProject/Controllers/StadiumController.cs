55256df9ff9b59cdb3c1d81c8df701f02bb399a0
Yeranuhi99 <termovsisyan99@gmail.com>
2023-09-28 00:49:00 +0400

    Stadium is working

--- MyFootballProject/Controllers/StadiumController.cs
+++ MyFootballProject/Controllers/StadiumController.cs
@@ -18,55 +18,55 @@ namespace MyFootballProject.Controllers
         {
             return View();
         }
-        //[HttpGet]
-        //public IActionResult AddEdit(int? id)
+        [HttpGet]
+        public IActionResult AddEdit(int? id)
+        {
+            StadiumAddEditVM model = id.HasValue ?
+                _stadiumService.GetById(id.Value) : new StadiumAddEditVM();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddEdit(StadiumAddEditVM club)
+        {
+            if (club.Id == 0)
+            {
+                _stadiumService.Add(club);
+            }
+            else
+            {
+                _stadiumService.Update(club);
+            }
+            return RedirectToAction(nameof(StadiumIndex));
+        }
+        //[httpget]
+        //public IActionResult Add()
         //{
-        //    StadiumAddEditVM model = id.HasValue ?
-        //        _stadiumService.GetById(id.Value) :new StadiumAddEditVM(); 
+        //    StadiumAddEditVM model = new StadiumAddEditVM();
         //    return View(model);
         //}
 
         //[HttpPost]
-        //public IActionResult AddEdit(StadiumAddEditVM club)
+        //public IActionResult Add(StadiumAddEditVM club)
         //{
-        //    if (club.Id == 0)
-        //    {
+
         //        _stadiumService.Add(club);
-        //    }
-        //    else
-        //    {
-        //        _stadiumService.Update(club);
-        //    }
+
         //    return RedirectToAction(nameof(StadiumIndex));
         //}
-        [HttpGet]
-        public IActionResult Add()
-        {
-            StadiumAddEditVM model = new StadiumAddEditVM();
-            return View(model);
-        }
 
-        [HttpPost]
-        public IActionResult Add(StadiumAddEditVM club)
-        {
-            
-                _stadiumService.Add(club);
-           
-            return RedirectToAction(nameof(StadiumIndex));
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            StadiumAddEditVM model = _stadiumService.GetById(id);
-            return View(model);
-        }
-        [HttpPost]
-        public IActionResult Edit(StadiumAddEditVM club)
-        {
-            _stadiumService.Update(club);
-            return RedirectToAction(nameof(StadiumIndex));
-        }
+        //[HttpGet]
+        //public IActionResult Edit(int id)
+        //{
+        //    StadiumAddEditVM model = _stadiumService.GetById(id);
+        //    return View(model);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(StadiumAddEditVM club)
+        //{
+        //    _stadiumService.Update(club);
+        //    return RedirectToAction(nameof(StadiumIndex));
+        //}
 
         public IActionResult Delete(int Id)
         { 
